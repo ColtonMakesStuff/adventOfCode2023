86d5f9b284b1d7f9634393e457eb9967f450aa3d
Colton Firestone <colton.firestone@gmail.com>
2023-12-04 14:17:37 -0800

    i honestly dont know how i did it in the end but i did it

--- 1-5/day4/index.js
+++ 1-5/day4/index.js
@@ -1,6 +1,11 @@
 import {findMatches} from "./logic.js";
 import {inputString} from "./data.js";
+import { findCardTotal } from "./logic.js";
+
 console.log("Hello World, this is Colton using js for the day 4 task");
 
-const matches = findMatches(inputString);
-console.log(matches);
+// const matches = findMatches(inputString);
+// console.log(matches);
+
+const cardTotal = findCardTotal(inputString);
+console.log(cardTotal);
--- 1-5/day4/logic.js
+++ 1-5/day4/logic.js
@@ -89,3 +89,107 @@ total += miniWinner;
 return total;
 }
 console.log(findMatches(exampleData));
+
+
+
+
+export const findCardTotal = (string) => {
+//  you win copies of the scratchcards below the winning card 
+//  the anount of cards to copy is equal to the number of matches
+//  for each match you win you get a copy of the cards below it 
+//  Copies of scratchcards are scored like normal scratchcards and have the same card number as the card they copied
+//  i thinking some sort of nesteed loop to check each card against the cards below it
+// maybe i can do some example problems first
+
+//for each card
+// check for matches
+// if are matches then add the number of matches to the total
+// for each match
+// check the match amount of cards below it for matches
+
+// inititalise total
+let total = 0;
+// split each string into an array of strings
+string = splitStringByPipes(string);
+console.log(string);
+let cardArray = [];
+// loop through each card
+for (let i = 0; i < string.length; i++) {
+
+// inititalise matches
+let matches = 0
+// regex to find numbers in the string
+let regex = /(\d+)/g;
+
+// inititalise arrays to store the numbers in each part of the card
+      let arrayPartOne = [];
+      let arrayPartTwo = [];
+      // inititalise match
+  let match;
+
+  // find all the numbers in the first part of the card and push them into an array
+  while ((match = regex.exec(string[i][0])) !== null) {
+    let number = `${parseInt(match)}`;
+    arrayPartOne.push(number);
+  }
+
+  // find all the numbers in the second part of the card and push them into an array
+  while ((match = regex.exec(string[i][1])) !== null) {
+    let number = `${parseInt(match)}`;
+    arrayPartTwo.push(number);
+  }
+  // console.log(arrayPartOne);
+  // console.log(arrayPartTwo);
+  
+// loop through the second part of the card and check if the numbers are in the first part of the card
+for (let j = 0; j < arrayPartTwo.length; j++) {
+ if (arrayPartOne.includes(arrayPartTwo[j])) {
+  //  if there is a match then add one to the matches
+   matches += 1;
+ }
+}
+cardArray.push(
+  {
+    cardNumber: i + 1,
+    value: matches,
+    instanceOf: 1
+  }
+);
+
+}
+//for each of the cards
+let cardTotal = 0;
+for (let i = 0; i < cardArray.length; i++) {
+  // i want to take the next series of cards equal to the value of the card
+  // then i want to increase their instanceOf value by the intanceOf value of the card
+  // i want to add the instanceOf value of the card to the total
+  // console.log(
+  //   `_________________________________________________________
+
+  //   i am on card ${cardArray[i].cardNumber}
+  //   it has a value of: ${cardArray[i].value} 
+  //   and an instance of: ${cardArray[i].instanceOf}`);
+    let cardToCheck = i + 1;
+    if (cardArray[i].value > 0) {
+for (let j = 1; j <= cardArray[i].value; j++) {
+  // console.log(`i am on card ${cardToCheck}`);
+  if (cardToCheck < cardArray.length) {
+    cardArray[cardToCheck].instanceOf += cardArray[i].instanceOf;
+    cardToCheck += 1;
+}
+}
+}
+cardTotal += cardArray[i].instanceOf;
+
+// console.log(`i checked ${cardArray[i].value} cards`);
+}
+// console.log(cardTotal);
+let test = "this is a test";
+return cardTotal;
+
+
+
+}
+
+console.log(findCardTotal(exampleData));
+
